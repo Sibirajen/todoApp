bf61c61167dc57814a8498d74fa19af10bd0dcdc
sibirajen <sibirajen16@gmail.com>
2025-01-27 12:50:39 +0530

    Added static getNumber method and removed redundant code

--- toDoApp/src/toDoApp/TaskCLI.java
+++ toDoApp/src/toDoApp/TaskCLI.java
@@ -1,6 +1,16 @@
 package toDoApp;
 
 public class TaskCLI {
+	
+	public static int getNumber(String number) {
+		try{
+			return Integer.parseInt(number);
+		}
+		catch(Exception e) {
+			System.out.println("Id needs to be a number");
+			return -1;
+		}
+	}
 
 	public static void main(String[] args) {
 		TaskManager taskManager = new TaskManager();
@@ -12,73 +22,68 @@ public class TaskCLI {
 		
 		String command = args[0];
 		switch(command) {
-			case "add":
+			case "add":{
 				if(args.length < 2) {
 					System.out.println("Usage: TaskCLI add <description>");
 					return;
 				}
 				taskManager.add(args[1]);
 				break;
-			case "update":
+			}
+			case "update":{
 				if(args.length < 3) {
 					System.out.println("Usage: TaskCLI update ID <description>");
 					return;
 				}
-				try {
-					int id = Integer.parseInt(args[1]);
+				int id = getNumber(args[1]);
+				if(id > -1) {
 					taskManager.update(id,args[2]);
 				}
-				catch(Exception e) {
-					System.out.println("ID needs to be a number");
-				}
 				break;
-			case "delete":
+			}
+			case "delete":{
 				if(args.length < 2) {
 					System.out.println("Usage: TaskCLI delete ID");
 					return;
 				}
-				try {
-					int id = Integer.parseInt(args[1]);
+				int id = getNumber(args[1]);
+				if(id > -1) {
 					taskManager.delete(id);
 				}
-				catch(Exception e) {
-					System.out.println("ID needs to be a number");
-				}
 				break;
-			case "mark-in-progress":
+			}
+			case "mark-in-progress":{
 				if(args.length < 2) {
 					System.out.println("Usage: TaskCLI mark-in-progress ID");
 					return;
 				}
-				try {
-					int id = Integer.parseInt(args[1]);
+				int id = getNumber(args[1]);
+				if(id > -1) {
 					taskManager.markInProgress(id);
 				}
-				catch(Exception e) {
-					System.out.println("ID needs to be a number");
-				}
 				break;
-			case "mark-done":
+			}
+			case "mark-done":{
 				if(args.length < 2) {
 					System.out.println("Usage: TaskCLI mark-done ID");
 					return;
 				}
-				try {
-					int id = Integer.parseInt(args[1]);
+				int id = getNumber(args[1]);
+				if(id > -1) {
 					taskManager.markDone(id);
 				}
-				catch(Exception e) {
-					System.out.println("ID needs to be a number");
-				}
 				break;
-			case "list":
+			}
+			case "list":{
 				if(args.length == 1) {
 					taskManager.list();
 				}
 				break;
-			default:
+			}
+			default:{
 				System.out.println("Usage: TaskCLI <command> [arguments]");
 				break;
+			}
 		}
 		taskManager.saveTask();
 	}
